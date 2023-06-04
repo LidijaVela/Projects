d67b5ea314d86c63a9d7ab2c83ba3cedc26dd86d
LidijaVela <121055446+LidijaVela@users.noreply.github.com>
2023-06-04 21:42:01 +0200

    Add files via upload

---
+++ PyFlora-novo/GUI/charts.py
@@ -0,0 +1,18 @@
+# # importing package
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# # create data
+# x = [1, 2, 3, 4, 5]
+# y = [3, 3, 3, 3, 3]
+
+# # plot lines
+# plt.plot(x, y, label="line 1")
+# plt.plot(y, x, label="line 2")
+# plt.plot(x, np.sin(x), label="curve 1")
+# plt.plot(x, np.cos(x), label="curve 2")
+
+
+# if __name__ == '__main__':
+#     plt.legend()
+#     plt.show()
